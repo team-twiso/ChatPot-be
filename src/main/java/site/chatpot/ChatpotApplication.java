c67a7fe5c74b6a86606c073d9c25baa659cd4386
hyeond0 <a98792530@gmail.com>
2024-07-23 11:24:01 +0900

    [feat] SpringBoot 초기 설정

---
+++ src/main/java/site/chatpot/ChatpotApplication.java
@@ -0,0 +1,13 @@
+package site.chatpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatpotApplication.class, args);
+	}
+
+}
