8c0308e513002b5c80d7ca7eba1852c4ec56d61f
hyeond0 <a98792530@gmail.com>
2024-07-26 14:58:42 +0900

    [feat] 공통 응답 및 예외 추가

---
+++ src/main/java/site/chatpot/domain/common/ResponseHeader.java
@@ -0,0 +1,24 @@
+package site.chatpot.domain.common;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ResponseHeader {
+    private boolean isSuccessful;
+    private int resultCode;
+    private String resultMessage;
+
+    @Builder
+    public ResponseHeader(boolean isSuccessful, int resultCode, String resultMessage) {
+        this.isSuccessful = isSuccessful;
+        this.resultCode = resultCode;
+        this.resultMessage = resultMessage;
+    }
+
+    public boolean getIsSuccessful() {
+        return isSuccessful;
+    }
+}
---
+++ src/main/java/site/chatpot/domain/common/exception/BaseException.java
@@ -0,0 +1,18 @@
+package site.chatpot.domain.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BaseException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public BaseException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
